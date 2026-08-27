4a463941f9cbdbc6da82bd9c4c8f775dab5c599d
isalu <isabellalucena15@gmail.com>
2026-08-27 12:31:34 -0300

    feat: implement SessionGuard for session token validation and authorization

---
+++ src/auth/guard/session.guard.ts
@@ -0,0 +1,34 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthService } from '../service/auth.service';
+
+@Injectable()
+export class SessionGuard implements CanActivate {
+  constructor(private readonly authService: AuthService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const sessionToken = request.headers['x-sesssion-token'];
+
+    if (!sessionToken) {
+      throw new UnauthorizedException('Session token required');
+    }
+
+    try {
+      const session = await this.authService.validateSessionToken(sessionToken);
+      if (!session.valid || !session.user) {
+        throw new UnauthorizedException('Invalid session token');
+      }
+
+      request.user = session.user;
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid session token');
+    }
+  }
+}
